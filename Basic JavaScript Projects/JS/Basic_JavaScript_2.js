07c76ce2896cea286421d5ed42d8ba311c9bfe32
Leo R <Leoramos1292@gmail.com>
2019-10-01 14:55:18 -0700

    add file and name updates

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_2.js
@@ -0,0 +1,12 @@
+function myFirstFunction() {
+    var words = " WHAT DID I JUST SAY!?";
+    var result = words.fontcolor("red")
+    document.getElementById("Red_Text").innerHTML = result;
+}
+
+function myFunction() {
+    var sentence = "I am learning";
+    sentence += " a lot from this book!";
+    document.getElementById("concatenate").innerHTML = sentence;
+}
+
