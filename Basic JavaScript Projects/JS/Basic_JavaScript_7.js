f607490701b7c6953bd89c0ded6c186455bb0e38
Leo <55296317+leoramos1292@users.noreply.github.com>
2019-10-03 16:02:56 -0700

    File updates

--- Basic JavaScript Projects/JS/Basic_JavaScript_7.js
+++ Basic JavaScript Projects/JS/Basic_JavaScript_7.js
@@ -11,8 +11,45 @@ console.log(math)
 
 console.log(math + f);
 
-function gotTime() {
-    if (new Date().getyear() > 1998) {
-        document.
+function got_Time() {
+    if (new Date().getHours() < 18) {
+        document.getElementById("time").innerHTML = "We party tonight. I am the machine."
     }
+}
+
+function birthYear() {
+    var year = document.getElementById("year").value;
+    if (year > 1998) {
+        document.getElementById("legal").innerHTML = "Boy you can't drink, get outta here."
+    }
+    if (year < 1998) {
+        document.getElementById("legal").innerHTML = "DRINKS ON ME FOOL!"
+
+    }
+}
+
+function drink_Game() {
+    drink = document.getElementById("drinks").value;
+    if (drink > 17) {
+        document.getElementById("drink").innerHTML = "You are respectable. You party with us tonight."
+    }
+    else {
+        document.getElementById("drink").innerHTML = "You are weak."
+
+    }
+}
+
+function Time_Function() {
+    var Time = new Date().getHours();
+    var Reply;
+    if (Time < 12 == Time > 0) {
+        Reply = "It is morning time!";
+    }
+    else if (Time > 12 == Time < 18) {
+        Reply = "It is the afternoon.";
+    }
+    else {
+        Reply = "It is the evening time.";
+    }
+    document.getElementById("Time_of_day").innerHTML = Reply;
 }
