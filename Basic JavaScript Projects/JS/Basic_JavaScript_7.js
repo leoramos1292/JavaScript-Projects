20efd797d86aec49416e60731098f81451d70105
Leo R <Leoramos1292@gmail.com>
2019-10-03 14:14:29 -0700

    update current changes

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_7.js
@@ -0,0 +1,18 @@
+var x = 100
+var b = 100
+
+function add() {
+    var f = 17
+}
+
+var math = x + b;
+
+console.log(math)
+
+console.log(math + f);
+
+function gotTime() {
+    if (new Date().getyear() > 1998) {
+        document.
+    }
+}
