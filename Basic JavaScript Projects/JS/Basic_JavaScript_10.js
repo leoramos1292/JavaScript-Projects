371ab395cb660a2efad5a3bef624feacbc815bca
Leo <55296317+leoramos1292@users.noreply.github.com>
2019-10-06 10:10:48 -0700

    Add Basic 10 files

--- Basic JavaScript Projects/JS/Basic_JavaScript_10.js
+++ Basic JavaScript Projects/JS/Basic_JavaScript_10.js
@@ -49,11 +49,43 @@ console.log(woah);
 var woah = "pal";
 console.log(woah);
 
-var l = add(5, 5);
+var L = add(5, 5);
 
 function add(f, u) {
     return f + u;
 }
-document.getElementById("adder").innerHTML = l;
+document.getElementById("adder").innerHTML = L;
 
 
+let gamers = {
+    brand: "Xbox",
+    iteration: " One",
+    year: 2013,
+    major_exclusive: " Halo? Not much this generation tbh :(",
+    describer : function() {
+    return "The " + this.brand + this.iteration + " was released in " + this.year + " and it's major exclusive was " + this.major_exclusive;
+    }  
+}
+document.getElementById("games").innerHTML = gamers.describer();
+
+var pop_Punk_Bands = ["New Found Glory", "Blink-182", "Neck Deep", "The Story So Far"];
+var wording = ""
+var G;
+function for_Looping() {
+    for (G = 0; G < pop_Punk_Bands.length; G++) {
+    if (G === 3) { break; }
+        wording += pop_Punk_Bands[G] + "<br>";
+    }
+    document.getElementById("List_of_bands").innerHTML = wording;
+}
+
+var fight_moves = ["punch", "kick", "take-down", "grapple"];
+var string = ""
+var K;
+function for_Looper() {
+    for (K = 0; K < fight_moves.length; K++) {
+    if (K === 2) { continue; }
+        string += fight_moves[K] + "<br>";  
+    }
+    document.getElementById("moves").innerHTML = string;
+}
