0c3037c916f6bbc186584afdee9757482c7e6676
Leo R <Leoramos1292@gmail.com>
2019-10-07 20:31:01 -0700

    basic 3 updates

---
+++ Javascript Projects/JavaScript/JavaScript_3.js
@@ -0,0 +1,4 @@
+function weight_Class(fighter) {
+    var MMAfighter = fighter.getAttribute("data-MMA_fighter");
+    alert(MMAfighter + " is a UFC fighter in the " + fighter.innerHTML + " division. He has wooped some serious butt so far.");
+} 
